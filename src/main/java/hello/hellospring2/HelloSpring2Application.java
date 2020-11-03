ee6e77ac896497408ec682d4dee4f0385341ed30
Yunok <ahyz0569@gmail.com>
2020-11-03 17:34:49 +0900

    create project

---
+++ src/main/java/hello/hellospring2/HelloSpring2Application.java
@@ -0,0 +1,13 @@
+package hello.hellospring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpring2Application.class, args);
+	}
+
+}
