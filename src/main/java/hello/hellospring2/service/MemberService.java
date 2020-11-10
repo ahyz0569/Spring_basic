e78729352a844a0f2ae72410fd1732b64f1a9e41
Yunok <ahyz0569@gmail.com>
2020-11-10 14:40:03 +0900

    회원관리예제 / 회원 서비스 개발

---
+++ src/main/java/hello/hellospring2/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring2.service;
+
+import hello.hellospring2.domain.Member;
+import hello.hellospring2.repository.MemberRepository;
+import hello.hellospring2.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+    회원가입
+     */
+    public Long join(Member member){
+        // 같은 이름이 있는 중복 회원X, Ctrl+Alt+M(Extract Method)
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+    
+    /*
+    전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findByID(memberId);
+    }
+
+
+}
