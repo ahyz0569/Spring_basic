ffcd89de2cc372c184f9b90eed7f232fa3c62edd
Yunok <ahyz0569@gmail.com>
2020-11-11 15:02:23 +0900

    회원 웹 기능 - 회원 등록

--- src/main/java/hello/hellospring2/controller/MemberController.java
+++ src/main/java/hello/hellospring2/controller/MemberController.java
@@ -1,17 +1,37 @@
 package hello.hellospring2.controller;
 
+import hello.hellospring2.domain.Member;
 import hello.hellospring2.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
 
-    private final MemberService memberService;
+    private MemberService memberService;
 
     // DI (의존성 주입)
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+
 }
---
+++ src/main/java/hello/hellospring2/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hellospring2.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
