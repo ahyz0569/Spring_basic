014640e0f3a6caa9f736ed4ce4855d6c4ef5e7b5
Yunok <ahyz0569@gmail.com>
2020-11-13 18:50:27 +0900

    스프링 DB 접근 기술 / JPA

--- src/main/java/hello/hellospring2/SpringConfig.java
+++ src/main/java/hello/hellospring2/SpringConfig.java
@@ -1,24 +1,22 @@
 package hello.hellospring2;
 
-import hello.hellospring2.repository.JdbcMemberRepository;
-import hello.hellospring2.repository.JdbcTemplateMemberRepository;
-import hello.hellospring2.repository.MemberRepository;
-import hello.hellospring2.repository.MemoryMemberRepository;
+import hello.hellospring2.repository.*;
 import hello.hellospring2.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private final EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -30,6 +28,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- src/main/java/hello/hellospring2/service/MemberService.java
+++ src/main/java/hello/hellospring2/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring2.repository.MemberRepository;
 import hello.hellospring2.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
